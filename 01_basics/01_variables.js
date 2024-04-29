5b2973728b824a2542cd6453dbc28be59c6e5ffb
Aditya Rathore <153427299+AdityaDRathore@users.noreply.github.com>
2024-04-29 15:45:03 +0000

    types of variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 14324; 
+var accountName = "AdityaR";
+let accountEmail = "aditya@gmail.com"; 
+accountCity = "Indore"; 
+let accountState; 
+
+console.table([accountEmail, accountName, accountId, accountCity, accountState]); 
